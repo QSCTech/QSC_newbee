af076aa558e344778504e3d56e2b298e326e8291
YangKeao <keao.yang@yahoo.com>
2017-06-16 16:16:00 +0800

    Turn to use Google Analysis

--- assets/js/main.js
+++ assets/js/main.js
@@ -293,14 +293,10 @@ $(document).ready(function () {
     })
 });
 
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?3d79002933b635ec32f076574d8e3887";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-if($(window).width()>1024){
-    $('.alert').css('display','block');
-}
+ga('create', 'UA-37991730-7', 'auto');
+ga('send', 'pageview');
